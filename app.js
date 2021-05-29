98d6144f1fb18951719ddb7dbe50265fe7f6f9e2
folathecoder <officialakinloyefolarin@gmail.com>
2021-05-29 16:26:12 +0100

    Accordion Logic ==== "Close Tabs When Body is Clicked"

--- app.js
+++ app.js
@@ -5,18 +5,26 @@
 const accordionContainer = document.querySelector('.accordion__container');
 const accordionHeader = document.querySelectorAll('.accordion__header');
 const accordionBody = document.querySelectorAll('.accordion__body-container');
+const body = document.body;
 
 
 //TODO: Initally close all accordion tabs
 
 //* Add active class to all accordion headers
-accordionHeader.forEach(accordionHead => {
+const headerActive = function () {
+    accordionHeader.forEach(accordionHead => {
     accordionHead.classList.add('active__header')
-});
+    });
+}
+headerActive();
+
 //* Add active class to all accordion bodies
-accordionBody.forEach(accordionBod => {
+const bodyActive = function () {
+    accordionBody.forEach(accordionBod => {
     accordionBod.classList.add('active__body')
-});
+    });
+}
+bodyActive();
 
 //TODO: Target the parent container using event delegation (target elements that contains "accordion__header") and listen for clicks
 
@@ -32,8 +40,22 @@ accordionContainer.addEventListener('click', e => {
         clicked.classList.toggle('active__header');
 
         //* Remove active class from the clicked accordion body
-        console.log(clicked.dataset.accordion);
         const contentBody = document.querySelector(`.accordion__body--${clicked.dataset.accordion}`);
         contentBody.classList.toggle('active__body');
     }
 })
+
+
+//TODO: Automatically close unused tabs when the body is clicked
+
+body.addEventListener('click', e => {
+
+    //* Target other parts of the page asides the accordion container and its children
+    
+    if (!e.target.closest('.accordion__container')) {
+        headerActive();
+        bodyActive();
+    }
+
+})
+
